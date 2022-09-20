98af7eca39227f233bd1d3367c8cf69c665de23b
supulkalhara <supul.19@cse.mrt.ac.lk>
2022-09-20 13:34:58 +0530

    removed unnecessary files

--- safetakeoff/src/App.js
+++ safetakeoff/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./components/LoginComponent";
+import Main from "./components/Main/MainComponent";
+import Home from "./components/Home/HomeComponent";
+// import EditAccountDetails from "./components/Edit Account Details/EditAccountDetails";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="main" element={<Main />}>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />}></Route>
+            {/* <Route
+              path="editaccountdetails"
+              element={<EditAccountDetails />}
+            ></Route> */}
+          </Route>
+       </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
