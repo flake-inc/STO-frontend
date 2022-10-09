92b4111e9ba67796ccb35f5826f2be4447f32395
supulkalhara <supul.19@cse.mrt.ac.lk>
2022-10-09 14:24:26 +0530

    removed unnecessary codes and files

--- safetakeoff/src/Components/Dashboard/Dashboard.jsx
+++ safetakeoff/src/Components/Dashboard/Dashboard.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Copyright from "../Public/Copyright/Copyright";
 import ResponsiveAppBar from "./ResponsiveAppBar";
-import CenteredTabs from "./CenteredTabs";
 import axios from "axios";
 
 import {
-  Avatar,
-  AvatarGroup,
   Box,
   Button,
   Grid,
   List,
-  ListItemAvatar,
   ListItemButton,
-  ListItemSecondaryAction,
   ListItemText,
   MenuItem,
   Stack,
@@ -22,11 +17,7 @@ import {
   Typography,
 } from "@mui/material";
 
-import OrdersTable from "./OrdersTable";
-import IncomeAreaChart from "./IncomeAreaChart";
-import MonthlyBarChart from "./MonthlyBarChart";
-import ReportAreaChart from "./ReportAreaChart";
-import SalesColumnChart from "./SalesColumnChart";
+
 import TempGauge from "./dailytempgauge";
 import WindGauge from "./DailyWindSpeedGauge";
 import PressureGauge from "./PressureGauge";
@@ -36,17 +27,10 @@ import YearlyTemp from "./TemperatureCharts/YearlyTemp";
 import TimevsTemperature from "./TemperatureCharts/TimevsTemperature";
 import MonthlyAvgWindSpeed from "./WindSpeedCharts/MonthlyAverageWindSpeed";
 import YearlyWind from "./WindSpeedCharts/YearlyWind";
-
 import MonthlyCloud from "./CloudCoverCharts/MonthlyCloud";
 import YearlyCloud from "./CloudCoverCharts/YearlyCloud";
-
 import MonthlyPressure from "./PressureCharts/MonthlyPressure";
 import YearlyPressure from "./PressureCharts/YearlyPressure";
-
-
-
-import AnalyticEcommerce from "../Cards/Statistics/AnalyticEcommerce";
-
 import MainCard from "../MainCard";
 
 import {
@@ -54,28 +38,6 @@ import {
   MessageOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { Container } from "@mui/system";
-// import avatar1 from 'assets/images/users/avatar-1.png';
-// import avatar2 from 'assets/images/users/avatar-2.png';
-// import avatar3 from 'assets/images/users/avatar-3.png';
-// import avatar4 from 'assets/images/users/avatar-4.png';
-
-// avatar style
-const avatarSX = {
-  width: 36,
-  height: 36,
-  fontSize: "1rem",
-};
-
-// action style
-const actionSX = {
-  mt: 0.75,
-  ml: 1,
-  top: "auto",
-  right: "auto",
-  alignSelf: "flex-start",
-  transform: "none",
-};
 
 // sales report status
 const status = [
@@ -131,22 +93,16 @@ function DashboardContent() {
           <Typography variant="h5">Today Overview:</Typography>
         </Grid>
         <Grid item xs={6} sm={3} md={2} lg={2} >
-          {/* <AnalyticEcommerce title="Temperature" count="2.56ºC" percentage={59.3} extra="35,000" /> */}
           <TempGauge />
         </Grid>
         <Grid item xs={6} sm={3} md={2} lg={2}>
-          {/* <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" /> */}
           <WindGauge />
         </Grid>
         <Grid item xs={6} sm={3} md={2} lg={2}>
           <CloudCover />
-
-          {/* <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" /> */}
         </Grid>
         <Grid item xs={6} sm={3} md={2} lg={2} spacing={2}>
           <PressureGauge />
-
-          {/* <AnalyticEcommerce title="Total Sales" count="$35,078" percentage={27.4} isLoss color="warning" extra="$20,395" /> */}
         </Grid>
       </Grid>
 
@@ -191,14 +147,6 @@ function DashboardContent() {
             </Grid>
             <Grid container rowSpacing={1.5} columnSpacing={5}>
 
-        
-        {/* <Grid item xs={6} sm={3} md={2} lg={2}>
-                <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
-            </Grid>
-            <Grid item xs={6} sm={3} md={2} lg={2}>
-                <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
-            </Grid> */}
-
         <Grid
           item
           md={8}
@@ -233,10 +181,6 @@ function DashboardContent() {
             </Grid>
           </Grid>
           <MainCard content={false} sx={{ mt: 1.5 }}>
-            {/* <Box sx={{ pt: 1, pr: 2 }}>
-                        <IncomeAreaChart slot={slot} />
-                    </Box> */}
-            {/* <MonthlyTemp/> */}
             {console.log(feature)}
             <GraphSelect feature={feature} slot={slot}/>
           </MainCard>
@@ -254,23 +198,6 @@ function DashboardContent() {
                 <Typography variant="h6" color="textSecondary">
                   Today's Statistics
                 </Typography>
-                <AnalyticEcommerce
-                  title="Maximum Temperature"
-                  count="34.2ºC"
-                  percentage={10.3}
-                  isLoss="true"
-                  extra="1.5°C"
-                  state="Temperature"
-                  event="increases"
-                />
-                <AnalyticEcommerce
-                  title="Minimum Temperature"
-                  count="12.56ºC"
-                  percentage={7.3}
-                  extra="2°C"
-                  state="Temperature"
-                  event="increases"
-                />
 
                 {/* <Typography variant="h4">Max Temperature: 34.2°C</Typography>
                             <Typography variant="h4">Min Temperature: 18.2°C</Typography> */}
@@ -322,15 +249,16 @@ function DashboardContent() {
         <Grid item xs={12} md={5} lg={4}>
           <Grid container alignItems="center" justifyContent="space-between">
             <Grid item>
-              <Typography variant="h5">Analytics Report</Typography>
+              <Typography variant="h5">Aircrafts</Typography>
             </Grid>
             <Grid item />
           </Grid>
           <MainCard sx={{ mt: 2 }} content={false}>
             <List sx={{ p: 0, "& .MuiListItemButton-root": { py: 2 } }}>
               <ListItemButton divider>
-                <ListItemText primary="Company Finance Growth" />
-                <Typography variant="h5">+45.14%</Typography>
+                <ListItemText primary="N187PH" />
+                <ListItemText secondary="SR20-G3" />
+                <Typography variant="h5">Single Engine Piston</Typography>
               </ListItemButton>
               <ListItemButton divider>
                 <ListItemText primary="Company Expenses Ratio" />
@@ -344,125 +272,8 @@ function DashboardContent() {
             {/* <ReportAreaChart /> */}
           </MainCard>
         </Grid>
-        {/* <Grid item xs={12} md={5} lg={4}>
-                <Grid container alignItems="center" justifyContent="space-between">
-                    <Grid item>
-                        <Typography variant="h5">Transaction History</Typography>
-                    </Grid>
-                    <Grid item />
-                </Grid>
-                <MainCard sx={{ mt: 2 }} content={false}>
-                    <List
-                        component="nav"
-                        sx={{
-                            px: 0,
-                            py: 0,
-                            '& .MuiListItemButton-root': {
-                                py: 1.5,
-                                '& .MuiAvatar-root': avatarSX,
-                                '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
-                            }
-                        }}
-                    >
-                        <ListItemButton divider>
-                            <ListItemAvatar>
-                                <Avatar
-                                    sx={{
-                                        color: 'success.main',
-                                        bgcolor: 'success.lighter'
-                                    }}
-                                >
-                                    <GiftOutlined />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={<Typography variant="subtitle1">Order #002434</Typography>} secondary="Today, 2:00 AM" />
-                            <ListItemSecondaryAction>
-                                <Stack alignItems="flex-end">
-                                    <Typography variant="subtitle1" noWrap>
-                                        + $1,430
-                                    </Typography>
-                                    <Typography variant="h6" color="secondary" noWrap>
-                                        78%
-                                    </Typography>
-                                </Stack>
-                            </ListItemSecondaryAction>
-                        </ListItemButton>
-                        <ListItemButton divider>
-                            <ListItemAvatar>
-                                <Avatar
-                                    sx={{
-                                        color: 'primary.main',
-                                        bgcolor: 'primary.lighter'
-                                    }}
-                                >
-                                    <MessageOutlined />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={<Typography variant="subtitle1">Order #984947</Typography>}
-                                secondary="5 August, 1:45 PM"
-                            />
-                            <ListItemSecondaryAction>
-                                <Stack alignItems="flex-end">
-                                    <Typography variant="subtitle1" noWrap>
-                                        + $302
-                                    </Typography>
-                                    <Typography variant="h6" color="secondary" noWrap>
-                                        8%
-                                    </Typography>
-                                </Stack>
-                            </ListItemSecondaryAction>
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemAvatar>
-                                <Avatar
-                                    sx={{
-                                        color: 'error.main',
-                                        bgcolor: 'error.lighter'
-                                    }}
-                                >
-                                    <SettingOutlined />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
-                            <ListItemSecondaryAction>
-                                <Stack alignItems="flex-end">
-                                    <Typography variant="subtitle1" noWrap>
-                                        + $682
-                                    </Typography>
-                                    <Typography variant="h6" color="secondary" noWrap>
-                                        16%
-                                    </Typography>
-                                </Stack>
-                            </ListItemSecondaryAction>
-                        </ListItemButton>
-                    </List>
-                </MainCard>
-                <MainCard sx={{ mt: 2 }}>
-                    <Stack spacing={3}>
-                        <Grid container justifyContent="space-between" alignItems="center">
-                            <Grid item>
-                                <Stack>
-                                    <Typography variant="h5" noWrap>
-                                        Help and Support Chat
-                                    </Typography>
-                                    <Typography variant="caption" color="secondary" noWrap>
-                                        Typical replay within 5 min
-                                    </Typography>
-                                </Stack>
-                            </Grid>
-                        </Grid>
-                        <Button size="small" variant="contained" sx={{ textTransform: 'capitalize' }}>
-                            Need Help?
-                        </Button>
-                        <Button size="small" variant="contained" sx={{ textTransform: 'capitalize' }}>
-                            {articles.dewpoint_temperature}
-                        </Button>
-                    </Stack>
-                </MainCard>
-            </Grid> */}
+
       </Grid>
-      {/* </Container> */}
       <Copyright />
     </>
   );
